3cf78a30916ea5f674d2a815d1354cee66aec057
thanhvan181 <nguyenthivan18012000@gmail.com>
2021-06-23 19:10:21 +0700

    second  commit

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -4,12 +4,11 @@ import classes from './MealsSummary.module.css'
 const MealsSummary = (props) => {
     return (
         <section className={classes.summary}>
-            <h2>Delicious Food, Delivered To You</h2>
+            <h2>Đồ ăn ngon, được giao cho bạn</h2>
             <p>
-                Choose your favorite meal from our broad selection of available meals
-                and enjoy a delicious lunch or dinner at home.</p>
+            Chọn bữa ăn yêu thích của bạn từ nhiều lựa chọn các bữa ăn sẵn có của chúng tôi và thưởng thức bữa trưa hoặc bữa tối ngon miệng tại nhà.</p>
             <p>
-                All our meals are cooked with hight-quality ingredients, just-in-time and of course by expenienced chefs!
+            Tất cả các bữa ăn của chúng tôi đều được nấu bằng những nguyên liệu chất lượng cao, đúng lúc và tất nhiên bởi những đầu bếp giàu kinh nghiệm!
             </p>
         </section>
     )
